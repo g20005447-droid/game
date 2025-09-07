5ccccf3241a0ff5e83528a087928a40949768d0c
gfffg_1111 <g20005447@gmail.com>
2025-09-07 10:46:27 +0300

    nado

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float x = 1f;
+        double y = 0.5;
+        var rovno1 = x + y;
+        var rovno2 = x - y;
+        var rovno3 = x * y;
+        var rovno4 = x / y;
+
+
+        System.out.println("сложение = " + rovno1);
+        System.out.println("вычитание = " + rovno2);
+        System.out.println("умножение = " + rovno3);
+        System.out.println("деление = " + rovno4);
+    }
+}
