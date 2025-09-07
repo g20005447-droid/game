40fc6bd63a329d5f551ba5e465e8de38cfabb19e
gfffg_1111 <g20005447@gmail.com>
2025-09-07 11:01:15 +0300

    nado

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        float x = 1f;
-        double y = 0.5;
+        // переменные
+        var x = 1;
+        var y = 0.5;
         var rovno1 = x + y;
         var rovno2 = x - y;
         var rovno3 = x * y;
         var rovno4 = x / y;
-
-
+        //____________________________________________
+        // действия
         System.out.println("сложение = " + rovno1);
         System.out.println("вычитание = " + rovno2);
         System.out.println("умножение = " + rovno3);
