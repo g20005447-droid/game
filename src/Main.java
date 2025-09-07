6ee2d074d577ff9d0ce6b7b95377862df51fa716
gfffg_1111 <g20005447@gmail.com>
2025-09-07 16:35:59 +0300

    nado

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,8 @@ public class Main {
         var rovno2 = x - y;
         var rovno3 = x * y;
         var rovno4 = x / y;
-        //____________________________________________
+        //byte (-128-127) short (-32 768-32 767) int (-2 147 483 648-2 147 483 647) long (-9 223 372 036 854 775 808-9 223 372 036 854 775 807)
+        //float (0.0000001) double (0.0000000000000001)
         // действия
         System.out.println("сложение = " + rovno1);
         System.out.println("вычитание = " + rovno2);
