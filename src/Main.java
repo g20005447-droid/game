834a01faa4d21bc62093c24f3a85322095dc1c7e
gfffg_1111 <g20005447@gmail.com>
2025-09-09 08:54:01 +0300

    nado

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,15 @@ public class Main {
         //byte (-128-127) short (-32 768-32 767) int (-2 147 483 648-2 147 483 647) long (-9 223 372 036 854 775 808-9 223 372 036 854 775 807)
         //float (0.0000001) double (0.0000000000000001)
         // действия
-        System.out.println("сложение = " + rovno1 + "       " + "вычитание = " + rovno2 + "       " + "умножение = " + rovno3 + "       " + "деление = " + rovno4);
+        print_all();
+        System.out.println("          ");
+        System.out.println("проверка мат. действий  :  " + "1 + 0.5 = " + rovno1 + "       " + "1 - 0.5 = " + rovno2 + "       " + "1 * 0.5 = " + rovno3 + "       " + "1 / 0.5 = " + rovno4);
+
+    }
+    // методы
+    private static void print_all() {
+        System.out.println("проверка методов");
     }
 }
+
+
