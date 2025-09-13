aa4f6f808df07650009f9815bd12cf368fc128de
gfffg_1111 <g20005447@gmail.com>
2025-09-13 17:20:47 +0300

    nado

--- src/Main.java
+++ src/Main.java
@@ -8,92 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         // переменные
-        var x = 1;
-        var y = 0.5;
-        var rovno1 = x + y;
-        var rovno2 = x - y;
-        var rovno3 = x * y;
-        var rovno4 = x / y;
-        int age = 18;
-        double firstNumber = 15;
-        double secondNumber = 25;
-        var operation = "*";
-        double result = 0;
-        var secretNumber = 45;
-        var playerGuess = 45;
+
         //byte (-128-127) short (-32 768-32 767) int (-2 147 483 648-2 147 483 647) long (-9 223 372 036 854 775 808-9 223 372 036 854 775 807)
         //float (0.0000001) double (0.0000000000000001) + && = and и || = or
         // switch (age) {case 18: default:} + if + String()?...:...;
         // действия
-        System.out.println("Проверка switch и string");
-        switch (age) {
-            case 18:
-                System.out.println("Ты совершенолетний" + " : это switch");
-
-                break;
-            default:
-                System.out.println("Ты не совершенолетний" + " : это switch");
-
-                break;
-
-        }
-        String answer = (age >= 18) ? "Ты совершенолетний": "Ты не совершенолетний";
-        System.out.println(answer  + " : это String");
-
-        PR();
-
-        if (age >= 18) {
-
-            qw();
-            System.out.println("Это if:");
-            System.out.println("Ты совершенолетний");
-            qw();
-            PR();
-        } else if (age < 18) {
-            qw();
-            System.out.println("Это if:");
-            System.out.println("Ты не совершенолетний");
-            qw();
-            if (age >= 7) {
-                var clases = age -= 6;
-                System.out.println("ты ходишь в " + clases + " класс");
-                PR();
-                qw();
-
-            } else if (age < 7 && age > 3) {
-                System.out.println("Ты ходишь в садик");
-                PR();
-            } else {
-                System.out.println("ты младенец");
-                PR();
-            }
-        }
-        print_all();
-        qw();
-        PR();
-        System.out.println("проверка мат. действий  :  " + "1 + 0.5 = " + rovno1 +"       1 - 0.5 = " + rovno2 + "       1 * 0.5 = " + rovno3 + "       1 / 0.5 = " + rovno4);
-        PR();
-
-
-        if (operation == "*") {
-            result = firstNumber * secondNumber;
-        } else if (operation == "+") {
-            result = firstNumber + secondNumber;
-        } else if (operation == "-") {
-            result = firstNumber - secondNumber;
-        } else if (operation == "/" && secondNumber != 0) {
-            result = firstNumber / secondNumber ;
-        } else {
-            System.out.println("ОШИБКА");
-        }
-
-
-        System.out.println(result);
-
-        String str = (secretNumber == playerGuess) ? "Поздравляем! Вы угадали число!": "Вы не угадали. Попробуйте снова!";
-        System.out.println(str);
-        PR();
-        System.out.println("конец");
 
     }
     // методы
